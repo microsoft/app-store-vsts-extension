5f39a8f0ad0b69b27340cc79c7b2fb93917e0e05
Ryu Yu <ryuyu@microsoft.com>
2016-02-26 16:17:39 -0800

    adding prelimiary bundle id extraction

--- Tasks/AppleAppStoreRelease/apple-app-store-release.js
+++ Tasks/AppleAppStoreRelease/apple-app-store-release.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var taskLibrary = require('vsts-task-lib');
+var ipaParser = require('ipa-metadata');
 
 // Get input variables
 var authType = taskLibrary.getInput('authType', false);
@@ -15,13 +16,15 @@ if (authType === "ServiceEndpoint") {
 
 var ipaPath = taskLibrary.getInput("ipaPath", true);
 var appName = taskLibrary.getInput("appName", true);
-var appIdentifier = taskLibrary.getInput("bundleId", true);
 var languageString = taskLibrary.getInput("language", true);
 var appVersion = taskLibrary.getInput("version", true);
 var shouldSkipItc = JSON.parse(taskLibrary.getInput("shouldSkipItc", false));
 var shouldSkipDevCenter = JSON.parse(taskLibrary.getInput("shouldSkipDevCenter", false));
+var overrideBundleIdentifier = taskLibrary.getInput("bundleId", false);
 var teamId = taskLibrary.getInput("teamId", false);
 var teamName = taskLibrary.getInput("teamName", false);
+
+var bundleIdentifier;
 
 // Set up environment
 var gemCache = process.env['GEM_CACHE'] || process.platform == 'win32' ? path.join(process.env['APPDATA'], 'gem-cache') : path.join(process.env['HOME'], '.gem-cache');
@@ -32,60 +35,80 @@ process.env['FASTLANE_DONT_STORE_PASSWORD'] = true;
 // Add bin of new gem home so we don't ahve to resolve it later;
 process.env['PATH'] = process.env['PATH'] + ":" + gemCache + path.sep + "bin";
 
-if (!appIdentifier) {
-    taskLibrary.setResult(1, "Name extraction from IPA failed. Is this a valid IPA file?");
-}
+ipaParser(ipaPath, function (err, extractedData) {
+    if (!overrideBundleIdentifier) {
+        if (err) {
+            taskLibrary.setResult(1, "IPA Parsing failed: " + err.message);
+        }
 
-installRubyGem("produce").then(function () {
-    // Setting up arguments for produce command
-    // See https://github.com/fastlane/produce for more information on these arguments
-    var args = [];
-    args.push("-u");
-    args.push(credentials.username);
-    args.push("-a");
-    args.push(appIdentifier);
-    args.push("-q");
-    args.push(appName);
-    args.push("-m");
-    args.push(languageString);
+        var metadata = extractedData.metadata;
 
-    if (shouldSkipItc) {
-        args.push("-i");
+        if (!metadata) {
+            taskLibrary.setResult(1, "Metadata is empty. Bundle ID extraction failed.");
+        }
+
+        bundleIdentifier = metadata.CFBundleIdentifier;
     }
 
-    if (shouldSkipDevCenter) {
-        args.push("-d");
+    if (!bundleIdentifier) {
+        bundleIdentifier = overrideBundleIdentifier;
     }
 
-    if (teamId) {
-        args.push("-b");
-        args.push(teamId);
-    }
-
-    if (teamName) {
-        args.push("-l");
-        args.push(teamName);
-    }
-
-    return runCommand("produce", args);
-}).then(function () {
-    return installRubyGem("deliver").then(function () {
-        // Setting up arguments for initializing deliver command
-        // See https://github.com/fastlane/deliver for more information on these arguments
-        var args = ["init"];
+    return installRubyGem("produce").then(function () {
+        // Setting up arguments for produce command
+        // See https://github.com/fastlane/produce for more information on these arguments
+        var args = [];
         args.push("-u");
         args.push(credentials.username);
         args.push("-a");
-        args.push(appIdentifier);
-        args.push("-i");
-        args.push(ipaPath);
+        args.push(bundleIdentifier);
+        args.push("-q");
+        args.push(appName);
+        args.push("-m");
+        args.push(languageString);
 
-        return runCommand("deliver", args).then(function () {
-            return runCommand("deliver", ["--force", "-i", ipaPath]);
+        if (shouldSkipItc) {
+            args.push("-i");
+        }
+
+        if (shouldSkipDevCenter) {
+            args.push("-d");
+        }
+
+        if (teamId) {
+            args.push("-b");
+            args.push(teamId);
+        }
+
+        if (teamName) {
+            args.push("-l");
+            args.push(teamName);
+        }
+
+        return runCommand("produce", args).fail(function (err) {
+            taskLibrary.setResult(1, err.message);
         });
+    }).then(function () {
+        return installRubyGem("deliver").then(function () {
+            // Setting up arguments for initializing deliver command
+            // See https://github.com/fastlane/deliver for more information on these arguments
+            var args = ["init"];
+            args.push("-u");
+            args.push(credentials.username);
+            args.push("-a");
+            args.push(appIdentifier);
+            args.push("-i");
+            args.push(ipaPath);
+
+            return runCommand("deliver", args).then(function () {
+                return runCommand("deliver", ["--force", "-i", ipaPath]).fail(function (err) {
+                    taskLibrary.setResult(1, err.message);
+                });
+            });
+        });
+    }).fail(function (err) {
+        taskLibrary.setResult(1, err.message);
     });
-}).fail(function (err) {
-    taskLibrary.setResult(1, err.message);
 });
 
 function installRubyGem(packageName, localPath) {
