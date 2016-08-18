c9bc4e7d873a17ce081538b87a910cee9f5e7d41
madhurig <madhurig@microsoft.com>
2016-08-18 12:16:40 -0400

    convert promote task to typescript + latest task-lib

--- Tasks/app-store-promote/app-store-promote.js
+++ Tasks/app-store-promote/app-store-promote.js
@@ -1,123 +1,80 @@
-var os = require('os');
-var path = require('path');
-var taskLibrary = require('vsts-task-lib');
-
-//check if this is running on Mac and fail the task if not
-if(os.platform() !== 'darwin') {
-    taskLibrary.setResult(1, 'App store promote can only be run from a Mac computer.');
-    taskLibrary.exit(1);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+const os = require('os');
+const path = require('path');
+const tl = require('vsts-task-lib/task');
+class UserCredentials {
+    UserCredentials() {
+    }
 }
-
-console.log("getting vars...");
-
-// Get input variables
-var authType = taskLibrary.getInput('authType', false);
-var credentials = {};
-if (authType === "ServiceEndpoint") {
-    var serviceEndpoint = taskLibrary.getEndpointAuthorization(taskLibrary.getInput("serviceEndpoint", true));
-    credentials.username = serviceEndpoint.parameters.username;
-    credentials.password = serviceEndpoint.parameters.password;
-} else if (authType == "UserAndPass") {
-    credentials.username = taskLibrary.getInput("username", true);
-    credentials.password = taskLibrary.getInput("password", true);
-}
-
-var appIdentifier = taskLibrary.getInput("appIdentifier", true);
-var buildNumber = taskLibrary.getInput("buildNumber", true);
-var shouldAutoRelease = JSON.parse(taskLibrary.getInput("shouldAutoRelease", false));
-var teamId = taskLibrary.getInput("teamId", false);
-var teamName = taskLibrary.getInput("teamName", false);
-
-console.log("gotallinputs");
-
-var appVersion;
-var appName;
-
-// Set up environment
-var gemCache = process.env['GEM_CACHE'] || process.platform == 'win32' ? path.join(process.env['APPDATA'], 'gem-cache') : path.join(process.env['HOME'], '.gem-cache');
-process.env['GEM_HOME'] = gemCache;
-process.env['FASTLANE_PASSWORD'] = credentials.password;
-process.env['FASTLANE_DONT_STORE_PASSWORD'] = true;
-
-// Add bin of new gem home so we don't ahve to resolve it later;
-process.env['PATH'] = process.env['PATH'] + ":" + gemCache + path.sep + "bin";
-
-return installRubyGem("deliver").then(function () {
-    // See https://github.com/fastlane/deliver for more information on these arguments
-    var args = [];
-    args.push("submit_build")
-    args.push("-u");
-    args.push(credentials.username);
-    args.push("-a");
-    args.push(appIdentifier);
-
-    if (buildNumber.toLowerCase() !== "latest") {
-        args.push("-n");
-        args.push(buildNumber);
-    }
-
-    if (shouldAutoRelease) {
-        args.push("--automatic_release");
-    }
-
-    if (teamId) {
-        args.push("-b");
-        args.push(teamId);
-    }
-
-    if (teamName) {
-        args.push("-l");
-        args.push(teamName);
-    }
-
-    args.push("--force");
-    return runCommand("deliver", args);
-}).fail(function (err) {
-    console.log("global error catch");
-    taskLibrary.setResult(1, err.message);
-    process.exit(1);
-});
-
-function installRubyGem(packageName, localPath) {
-    taskLibrary.debug("Checking for ruby install...");
-    taskLibrary.which("ruby", true);
-    taskLibrary.debug("Checking for gem install...");
-    taskLibrary.which("gem", true);
-
-    taskLibrary.debug("Setting up gem install");
-    var command = new taskLibrary.ToolRunner("gem");
-    command.arg("install");
-    command.arg(packageName);
-
-    if (localPath) {
-        command.arg("--install-dir");
-        command.arg(localPath);
-    }
-
-    taskLibrary.debug("Attempting to install " + packageName + " to " + (localPath ? localPath : " default cache directory (" + process.env['GEM_HOME'] + ")"));
-    return command.exec().fail(function (err) {
-        taskLibrary.debug('taskRunner failed with error ' + err.message);
-        throw err;
+exports.UserCredentials = UserCredentials;
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            //check if this is running on Mac and fail the task if not
+            if (os.platform() !== 'darwin') {
+                throw 'App store promote can only be run from a Mac computer.';
+            }
+            tl.debug('Read all inputs ...');
+            // Get input variables
+            var authType = tl.getInput('authType', false);
+            var credentials = new UserCredentials();
+            if (authType === 'ServiceEndpoint') {
+                var serviceEndpoint = tl.getInput('serviceEndpoint', true);
+                credentials.username = tl.getEndpointAuthorizationParameter(serviceEndpoint, 'username', true);
+                credentials.password = tl.getEndpointAuthorizationParameter(serviceEndpoint, 'password', true);
+            }
+            else if (authType == 'UserAndPass') {
+                credentials.username = tl.getInput('username', true);
+                credentials.password = tl.getInput('password', true);
+            }
+            var appIdentifier = tl.getInput('appIdentifier', true);
+            var buildNumber = tl.getInput('buildNumber', true);
+            var shouldAutoRelease = tl.getBoolInput('shouldAutoRelease', false);
+            var teamId = tl.getInput('teamId', false);
+            var teamName = tl.getInput('teamName', false);
+            tl.debug('Read all inputs.');
+            var appVersion;
+            var appName;
+            // Set up environment
+            var gemCache = process.env['GEM_CACHE'] || process.platform == 'win32' ? path.join(process.env['APPDATA'], 'gem-cache') : path.join(process.env['HOME'], '.gem-cache');
+            process.env['GEM_HOME'] = gemCache;
+            process.env['FASTLANE_PASSWORD'] = credentials.password;
+            process.env['FASTLANE_DONT_STORE_PASSWORD'] = true;
+            // Add bin of new gem home so we don't ahve to resolve it later;
+            process.env['PATH'] = process.env['PATH'] + ':' + gemCache + path.sep + 'bin';
+            //Install the ruby gem for fastlane deliver
+            tl.debug('Checking for ruby install...');
+            tl.which('ruby', true);
+            var installDeliver = tl.tool(tl.which('gem', true));
+            installDeliver.arg(['install', 'deliver']);
+            yield installDeliver.exec();
+            //Run the deliver command 
+            // See https://github.com/fastlane/deliver for more information on these arguments
+            var deliverCommand = tl.tool('deliver');
+            deliverCommand.arg(['submit_build', '-u', credentials.username, '-a', appIdentifier]);
+            if (buildNumber.toLowerCase() !== 'latest') {
+                deliverCommand.arg(['-n', buildNumber]);
+            }
+            deliverCommand.arg(['--skip_binary_upload', 'true', '--skip_metadata', 'true', '--skip_screenshots', 'true']);
+            deliverCommand.argIf(shouldAutoRelease, '--automatic_release');
+            deliverCommand.argIf(teamId, ['-b', teamId]);
+            deliverCommand.argIf(teamName, ['-l', teamName]);
+            deliverCommand.arg('--force');
+            yield deliverCommand.exec();
+            tl.setResult(tl.TaskResult.Succeeded, 'Build ' + buildNumber + ' successfully promoted.');
+        }
+        catch (err) {
+            tl.setResult(tl.TaskResult.Failed, err);
+        }
     });
 }
-
-function runCommand(commandString, args) {
-    taskLibrary.debug("Setting up command " + commandString);
-    if (typeof args == "string") {
-        args = [args];
-    }
-
-    var command = new taskLibrary.ToolRunner(commandString);
-
-    if (args) {
-        args.forEach(function (arg) {
-            taskLibrary.debug("Appending argument: " + arg);
-            command.arg(arg);
-        });
-    }
-
-    return command.exec().fail(function (err) {
-        taskLibrary.debug('taskRunner failed with error ' + err.message);
-        throw err;
-    });
-}
+run();
+//# sourceMappingURL=app-store-promote.js.map
---
+++ Tasks/app-store-promote/app-store-promote.ts
@@ -0,0 +1,85 @@
+import os = require('os');
+import path = require('path');
+import tl = require('vsts-task-lib/task');
+import {ToolRunner} from 'vsts-task-lib/toolrunner';
+
+export class UserCredentials {
+    username: string;
+    password: string;
+
+    public UserCredentials() {
+
+    }
+}
+
+async function run() {
+    try {
+        //check if this is running on Mac and fail the task if not
+        if(os.platform() !== 'darwin') {
+            throw 'App store promote can only be run from a Mac computer.';
+        }
+
+        tl.debug('Read all inputs ...');
+
+        // Get input variables
+        var authType = tl.getInput('authType', false);
+        var credentials : UserCredentials = new UserCredentials();
+        if (authType === 'ServiceEndpoint') {
+            var serviceEndpoint = tl.getInput('serviceEndpoint', true); 
+            credentials.username = tl.getEndpointAuthorizationParameter(serviceEndpoint, 'username', true);
+            credentials.password = tl.getEndpointAuthorizationParameter(serviceEndpoint, 'password', true);
+        } else if (authType == 'UserAndPass') {
+            credentials.username = tl.getInput('username', true);
+            credentials.password = tl.getInput('password', true);
+        }
+
+        var appIdentifier = tl.getInput('appIdentifier', true);
+        var buildNumber = tl.getInput('buildNumber', true);
+        var shouldAutoRelease = tl.getBoolInput('shouldAutoRelease', false);
+        var teamId = tl.getInput('teamId', false);
+        var teamName = tl.getInput('teamName', false);
+
+        tl.debug('Read all inputs.');
+
+        var appVersion;
+        var appName;
+
+        // Set up environment
+        var gemCache = process.env['GEM_CACHE'] || process.platform == 'win32' ? path.join(process.env['APPDATA'], 'gem-cache') : path.join(process.env['HOME'], '.gem-cache');
+        process.env['GEM_HOME'] = gemCache;
+        process.env['FASTLANE_PASSWORD'] = credentials.password;
+        process.env['FASTLANE_DONT_STORE_PASSWORD'] = true;
+
+        // Add bin of new gem home so we don't ahve to resolve it later;
+        process.env['PATH'] = process.env['PATH'] + ':' + gemCache + path.sep + 'bin';
+
+        //Install the ruby gem for fastlane deliver
+        tl.debug('Checking for ruby install...');
+        tl.which('ruby', true);
+        var installDeliver: ToolRunner = tl.tool(tl.which('gem', true));
+        installDeliver.arg(['install', 'deliver']);
+        await installDeliver.exec();
+        
+        //Run the deliver command 
+        // See https://github.com/fastlane/deliver for more information on these arguments
+        var deliverCommand : ToolRunner = tl.tool('deliver');
+        deliverCommand.arg(['submit_build', '-u', credentials.username, '-a', appIdentifier]);
+        if (buildNumber.toLowerCase() !== 'latest') {
+           deliverCommand.arg(['-n', buildNumber]);     
+        }
+        deliverCommand.arg(['--skip_binary_upload', 'true', '--skip_metadata', 'true', '--skip_screenshots', 'true']);
+        deliverCommand.argIf(shouldAutoRelease, '--automatic_release');
+        deliverCommand.argIf(teamId, ['-b', teamId]);
+        deliverCommand.argIf(teamName, ['-l', teamName]);
+        deliverCommand.arg('--force');
+
+        await deliverCommand.exec();
+ 
+        tl.setResult(tl.TaskResult.Succeeded, 'Build ' + buildNumber + ' successfully promoted.');
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err);
+    }
+}
+
+run();
