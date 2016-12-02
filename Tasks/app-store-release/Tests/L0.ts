d6ab51dbff70e1bbc2a7fe58cf658b58bef4c7b4
Jeff Young <jeyou@microsoft.com>
2016-12-02 14:40:10 -0500

    Add more unit tests

--- Tasks/app-store-release/Tests/L0.ts
+++ Tasks/app-store-release/Tests/L0.ts
@@ -27,10 +27,50 @@ describe('app-store-release L0 Suite', function () {
         done();
     });
 
-    it('username+password with pilot', (done:MochaDone) => {
+    it('no authtype', (done:MochaDone) => {
         this.timeout(1000);
 
-        let tp = path.join(__dirname, 'L0UserPassPilot.js');
+        let tp = path.join(__dirname, 'L0NoAuthType.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.stdout.indexOf('Input required: authType') !== -1, 'Task should have written to stdout');
+        assert(tr.failed, 'task should have failed');
+
+        done();
+    });
+
+    it('no service endpoint', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0NoEndpoint.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.stdout.indexOf('Input required: serviceEndpoint') !== -1, 'Task should have written to stdout');
+        assert(tr.failed, 'task should have failed');
+
+        done();
+    });
+
+    it('no username+password', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0NoUserPass.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        // When no username+password is provided, username fails first
+        assert(tr.stdout.indexOf('Input required: username') !== -1, 'Task should have written to stdout');
+        assert(tr.failed, 'task should have failed');
+
+        done();
+    });
+
+    it('testflight - username+password', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightUserPass.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
         tr.run();
@@ -39,10 +79,10 @@ describe('app-store-release L0 Suite', function () {
         done();
     });
 
-    it('service endpoint with pilot', (done:MochaDone) => {
+    it('testflight - service endpoint', (done:MochaDone) => {
         this.timeout(1000);
 
-        let tp = path.join(__dirname, 'L0ServiceEndpointPilot.js');
+        let tp = path.join(__dirname, 'L0TestFlightServiceEndpoint.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
         tr.run();
@@ -50,5 +90,256 @@ describe('app-store-release L0 Suite', function () {
 
         done();
     });
+
+    it('testflight - no ipa path', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightNoIpaPath.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.stdout.indexOf('Input required: ipaPath') !== -1, 'Task should have written to stdout');
+        assert(tr.failed, 'task should have failed');
+
+        done();
+    });
+
+    // it('testflight - release notes', (done:MochaDone) => {
+    //     this.timeout(1000);
+
+    //     let tp = path.join(__dirname, 'L0TestFlightReleaseNotes.js');
+    //     let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+    //     tr.run();
+    //     //assert(tr.stderr.indexOf('Input required: ipaPath') !== -1, 'Task should have written to stderr');
+    //     //assert(tr.stdout.indexOf('Input required: ipaPath') !== -1, 'Task should have written to stdout');
+    //     assert(tr.succeeded, 'task should have succeeded');
+
+    //     done();
+    // });
+
+    it('testflight - team id', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightTeamId.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('pilot upload -u creds-username -i <path> -q teamId'), 'pilot upload with teamId should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and pilot.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('testflight - team name', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightTeamName.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('pilot upload -u creds-username -i <path> -r teamName'), 'pilot upload with teamName should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and pilot.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('testflight - team id and team name', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightTeamIdTeamName.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('pilot upload -u creds-username -i <path> -q teamId -r teamName'), 'pilot upload with teamId and teamName should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and pilot.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('testflight - should skip submission', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightShouldSkipSubmission.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('pilot upload -u creds-username -i <path> --skip_submission true'), 'pilot upload with skip_submission should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and pilot.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('testflight - should skip waiting for processing', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0TestFlightShouldSkipWaitingForProcessing.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('pilot upload -u creds-username -i <path> --skip_waiting_for_build_processing true'), 'pilot upload with skip_waiting_for_build_processing should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and pilot.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - no bundle id', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionNoBundleId.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.stdout.indexOf('Input required: appIdentifier') !== -1, 'Task should have written to stdout');
+        assert(tr.failed, 'task should have failed');
+
+        done();
+    });
+
+    it('production - no ipa path', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionNoIpaPath.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.stdout.indexOf('Input required: ipaPath') !== -1, 'Task should have written to stdout');
+        assert(tr.failed, 'task should have failed');
+
+        done();
+    });
+
+    it('production - should skip binary upload', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionShouldSkipBinaryUpload.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_binary_upload true --skip_metadata true --skip_screenshots true'), 'deliver with skip_binary_upload should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - team id', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionTeamId.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_metadata true --skip_screenshots true -q teamId'), 'deliver with teamId should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - team name', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionTeamName.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_metadata true --skip_screenshots true -r teamName'), 'deliver with teamName should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - team id and team name', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionTeamIdTeamName.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_metadata true --skip_screenshots true -q teamId -r teamName'), 'deliver with teamId and teamName should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - should submit for review', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionShouldSubmitForReview.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_metadata true --skip_screenshots true --submit_for_review true'), 'deliver with submit_for_review should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - automatic release', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionShouldAutoRelease.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_metadata true --skip_screenshots true --automatic_release true'), 'deliver with automatic_release should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - upload metadata with metadata path', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionUploadMetadataMetadataPath.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> -m <path> --skip_screenshots true'), 'deliver with -m should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    it('production - upload screenshots with screenshots path', (done:MochaDone) => {
+        this.timeout(1000);
+
+        let tp = path.join(__dirname, 'L0ProductionUploadScreenshotsScreenshotsPath.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.ran('deliver --force -u creds-username -a com.microsoft.test.appId -i <path> --skip_metadata true -w <path>'), 'deliver with -w should have been run.');
+        assert(tr.invokedToolCount === 2, 'should have run both gem and deliver.');
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
+    //No tests for every combination of uploadMetadata and metadataPath (one true, one false)
+    //No tests for every combination of uploadScreenshots and screenshotsPath (one true, one false)
 
 });
---
+++ Tasks/app-store-release/Tests/L0ProductionUploadMetadataMetadataPath.ts
@@ -0,0 +1,52 @@
+import ma = require('vsts-task-lib/mock-answer');
+import tmrm = require('vsts-task-lib/mock-run');
+import path = require('path');
+import os = require('os');
+
+let taskPath = path.join(__dirname, '..', 'app-store-release.js');
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tmr.setInput('authType', 'UserAndPass');
+tmr.setInput('username', 'creds-username');
+tmr.setInput('password', 'creds-password');
+tmr.setInput('releaseTrack', 'Production');
+tmr.setInput('ipaPath', '<path>');
+tmr.setInput('appIdentifier', 'com.microsoft.test.appId');
+
+tmr.setInput('uploadMetadata', 'true');
+tmr.setInput('metadataPath', '<path>');
+
+// provide answers for task mock
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers> {
+    'which': {
+        'ruby': '/usr/bin/ruby',
+        'gem': '/usr/bin/gem',
+        'deliver': '/usr/bin/deliver',
+        'pilot': '/usr/bin/pilot'
+    },
+    'checkPath' : {
+        '/usr/bin/ruby': true,
+        '/usr/bin/gem': true,
+        '/usr/bin/deliver': true,
+        '/usr/bin/pilot': true
+    },
+    'exec': {
+        '/usr/bin/gem install deliver': {
+            'code': 0,
+            'stdout': 'truly outrageous!'
+        },
+        'deliver --force -u creds-username -a com.microsoft.test.appId -i <path> -m <path> --skip_screenshots true': {
+            'code': 0,
+            'stdout': 'truly outrageous!'
+        }
+    }
+};
+tmr.setAnswers(a);
+
+// This is how you can mock NPM packages...
+os.platform = () => {
+    return 'darwin';
+};
+tmr.registerMock('os', os);
+
+tmr.run();
