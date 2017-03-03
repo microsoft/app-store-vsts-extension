0a9d5f21704d37d2bec69b622f3af8fc7cc8edaa
Jeff Young (TFS) <jeyou@microsoft.com>
2017-03-03 13:51:24 -0500

    Add App Store Promote tests to test fastlane install

---
+++ Tasks/app-store-promote/Tests/L0SpecificFastlaneInstallNoVersion.ts
@@ -0,0 +1,57 @@
+ /*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import ma = require('vsts-task-lib/mock-answer');
+import tmrm = require('vsts-task-lib/mock-run');
+import path = require('path');
+import os = require('os');
+
+let taskPath = path.join(__dirname, '..', 'app-store-promote.js');
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tmr.setInput('authType', 'UserAndPass');
+tmr.setInput('username', 'creds-username');
+tmr.setInput('password', 'creds-password');
+tmr.setInput('appIdentifier', 'com.microsoft.test.appId');
+tmr.setInput('chooseBuild', 'latest');
+tmr.setInput('shouldAutoRelease', 'true');
+tmr.setInput('installFastlane', 'true');
+tmr.setInput('fastlaneToolsVersion', 'SpecificVersion');
+//tmr.setInput('fastlaneToolsSpecificVersion', '2.15.1');
+
+process.env['MOCK_NORMALIZE_SLASHES'] = true;
+process.env['HOME'] = '/usr/bin';
+
+//construct a string that is JSON, call JSON.parse(string), send that to ma.TaskLibAnswers
+let myAnswers: string = `{
+    "which": {
+        "ruby": "/usr/bin/ruby",
+        "gem": "/usr/bin/gem",
+        "fastlane": "/usr/bin/fastlane"
+    },
+    "checkPath" : {
+        "/usr/bin/ruby": true,
+        "/usr/bin/gem": true,
+        "/usr/bin/fastlane": true
+    },
+    "exec": {
+        "fastlane deliver submit_build -u creds-username -a com.microsoft.test.appId --skip_binary_upload true --skip_metadata true --skip_screenshots true --automatic_release --force": {
+            "code": 0,
+            "stdout": "consider it delivered!"
+        }
+    }
+ }`;
+let json: any = JSON.parse(myAnswers);
+// Cast the json blob into a TaskLibAnswers
+tmr.setAnswers(<ma.TaskLibAnswers>json);
+
+// This is how you can mock NPM packages...
+os.platform = () => {
+    return 'darwin';
+};
+tmr.registerMock('os', os);
+
+tmr.run();
