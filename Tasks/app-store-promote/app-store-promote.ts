97565f875341196e0519b90aee0f94efbfa62c49
luizgrs <luizgrs@gmail.com>
2020-08-17 18:06:11 -0300

    use toolrunner.line to support multiple arguments

--- Tasks/app-store-promote/app-store-promote.ts
+++ Tasks/app-store-promote/app-store-promote.ts
@@ -152,7 +152,10 @@ async function run() {
         deliverCommand.argIf(teamId, ['-k', teamId]);
         deliverCommand.argIf(teamName, ['-e', teamName]);
         deliverCommand.arg('--force');
-        deliverCommand.argIf(fastlaneArguments, fastlaneArguments);
+
+        //use .line instead of arg/argif to support mulitple parameters input by user
+        if(fastlaneArguments)
+            deliverCommand.line(fastlaneArguments);
 
         await deliverCommand.exec();
 
