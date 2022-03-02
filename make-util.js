dd62bc736f34ff31670e9c1aef4d3181746a1d99
Ivan Golubev <v-igolubev@microsoft.com>
2022-03-02 22:21:42 +0300

    Update make-util.js

--- make-util.js
+++ make-util.js
@@ -1145,23 +1145,6 @@ exports.toOverrideString = toOverrideString;
 var createExtension = function(manifest) {
     ensureTool('tsc', '--version', 'Version 3.2.2');
     ensureTool('mocha', '--version', '5.2.0');
-
-    console.log('Installing task dependencies...');
-
-    var rootPath = process.cwd(); 
-    var tasksPath = path.join(rootPath, 'Tasks');
-    var tasks = fs.readdirSync(tasksPath);
-    console.log(tasks.length + ' tasks found.')
-    tasks.forEach(function(task) {
-        console.log('Processing task ' + task);
-        cd(path.join(tasksPath,task));
-
-        console.log('Installing PRODUCTION npm dependencies for task (' + task + ')...');
-
-        run('npm install --only=prod');
-    });
-
-    cd(rootPath);
     
     matchRemove('**/Tests', path.join(__dirname, '_build/Tasks/'));
     matchRemove('**/*.js.map', path.join(__dirname, '_build/Tasks/'));
