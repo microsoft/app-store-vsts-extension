52282bdc07660fd6bc7797e1cbe30611656814cb
surajitshil-03 <surajitshil@microsoft.com>
2026-01-20 14:54:05 +0530

    Node24 changes in Apple Extension Tasks (#409)
    **Task name**: 
    AppStorePromote
    AppStoreRelease
    IpaResign
    
    Related work-item: AB#2349554
    
    **Description**:
    Adding Node24 related changes to the tasks and also updating the version
    of important packages such as task-lib, typescript version, node version
    etc. Also updated the typescript version in the
    dev-dependencies-constants.js file.
    
    **Documentation changes required:** (Y/N)
    No
    
    **Added unit tests:** (Y/N) 
    No
    
    **Attached related issue:** (Y/N) <Please add link to related issue
    here>
    
    **Checklist**:
    - [x] Task version was bumped - please check
    [instruction](https://github.com/microsoft/azure-pipelines-tasks/blob/master/docs/taskversionbumping.md)
    how to do it
    - [x] Checked that applied changes work as expected

--- dev-dependencies-constants.js
+++ dev-dependencies-constants.js
@@ -8,7 +8,7 @@ exports.MOCHA_TARGET_VERSION = '10.7.3';
 /** Minimum node version for MOCHA_TARGET_VERSION */
 exports.MOCHA_NODE_VERSION = 14;
 /** Current typescript version */
-exports.TSC_CURRENT_VERSION = '5.1.6';
+exports.TSC_CURRENT_VERSION = '5.7.2';
 /** Minimum supported node version */
 exports.NODE_MIN_VERSION = '10.0.0';
 /** Minimum supported npm version */
